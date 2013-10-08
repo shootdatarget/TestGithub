3dde239e279748bfcba4c73143a26e12f38ac12f
shootdatarget <amran@myrdrs.com>
2013-10-08 16:54:19 +0800

    i just dragg it all

--- TestGithub/src/my/shootdatarget/testgithub/MainActivity.java
+++ TestGithub/src/my/shootdatarget/testgithub/MainActivity.java
@@ -2,14 +2,31 @@ package my.shootdatarget.testgithub;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button go = (Button) findViewById(R.id.go); 
+		go.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this,SecondGozaru.class));
+			}
+		});
+		
 	}
 
 	@Override
---
+++ TestGithub/src/my/shootdatarget/testgithub/SecondGozaru.java
@@ -0,0 +1,16 @@
+package my.shootdatarget.testgithub;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+
+public class SecondGozaru extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.secondgozaru);
+	}
+
+}
